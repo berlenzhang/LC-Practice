9ded310522c4c01073761212d3f530d60d93ab2c
berlenzhang <146789528+berlenzhang@users.noreply.github.com>
2026-07-14 14:42:47 -0500

    backtracking folder and backtracking question

---
+++ LC-150/Backtracking/LetterComboOfPhoneNum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hashmap = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', 
+            '7':'pqrs', '8':'tuv', '9': 'wxyz'}
+
+        res = []
+        def backtracking(i, cur):
+            if i == len(digits):
+                res.append(cur)
+                return
+            
+            for c in hashmap[digits[i]]:
+                backtracking(i + 1, cur + c)
+
+        if digits:
+            backtracking(0, "")
+
+        return res
