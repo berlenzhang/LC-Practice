047acda8e66e68d9f74c78fa3f717e7b9bfeb46f
berlenzhang <146789528+berlenzhang@users.noreply.github.com>
2026-08-01 23:11:20 -0500

    Students and Examinations added to SQL 50

---
+++ SQL-50/Easy/StudentsAndExams/StudentsAndExams.sql
@@ -0,0 +1,8 @@
+SELECT t1.student_id, t1.student_name, t2.subject_name, COUNT(t3.student_id) as attended_exams
+FROM Students t1 CROSS JOIN Subjects t2 
+LEFT JOIN Examinations t3 
+    ON t1.student_id = t3.student_id 
+    AND t2.subject_name = t3.subject_name
+
+ GROUP BY t1.student_id, t1.student_name, t2.subject_name
+ ORDER BY t1.student_id, t2.subject_name
